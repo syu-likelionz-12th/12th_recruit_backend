19971dee250432ac99359de8be738eb0281c55e3
sungjun4403 <geulligu89@naver.com>
2024-01-09 19:19:46 +0900

    🎉 : initilize

---
+++ syu_likelion_official/src/main/java/com/example/syu_likelion_official/SyuLikelionOfficialApplication.java
@@ -0,0 +1,13 @@
+package com.example.syu_likelion_official;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyuLikelionOfficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyuLikelionOfficialApplication.class, args);
+	}
+
+}
---
+++ syu_likelion_official/src/test/java/com/example/syu_likelion_official/SyuLikelionOfficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syu_likelion_official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyuLikelionOfficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
