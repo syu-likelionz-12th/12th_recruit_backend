5e6446977f1336633b128649899b763323b58cd4
sungjun4403 <geulligu89@naver.com>
2024-01-10 03:21:26 +0900

    🔨, 🔧 : add file structure

---
+++ syu_likelion_official/src/main/java/com/example/syu_likelion_official/domain/recruit_form/controller/RecruitFormController.java
@@ -0,0 +1,4 @@
+package com.example.syu_likelion_official.domain.recruit_form.controller;
+
+public class RecruitFormController {
+}
---
+++ syu_likelion_official/src/main/java/com/example/syu_likelion_official/global/config/CorsConfig.java
@@ -0,0 +1,4 @@
+package com.example.syu_likelion_official.global.config;
+
+public class CorsConfig {
+}
