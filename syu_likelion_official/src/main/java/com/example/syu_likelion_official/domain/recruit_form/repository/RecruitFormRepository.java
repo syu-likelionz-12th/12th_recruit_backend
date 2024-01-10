66edc0b06be2a8f10fddd6d3818fea675f46f0af
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-01-11 02:57:45 +0900

    create!!><

---
+++ syu_likelion_official/src/main/java/com/example/syu_likelion_official/domain/recruit_form/repository/RecruitFormRepository.java
@@ -0,0 +1,8 @@
+package com.example.syu_likelion_official.domain.recruit_form.repository;
+
+import com.example.syu_likelion_official.domain.recruit_form.entity.RecruitForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitFormRepository extends JpaRepository<RecruitForm, Long> {
+
+}
